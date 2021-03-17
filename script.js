33f6a7c5a45a1dcd5f3b1f7db9a74df0bf097f33
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2021-03-17 19:52:57 +0300

    code changed

--- script.js
+++ script.js
@@ -1,98 +1,100 @@
+const navItems = document.querySelectorAll('.nav__item');
+const btnStart = document.querySelector('.btn-start');
 const startMenu = document.getElementById('start-menu');
 const gameContainer = document.getElementById('game-container');
-const navButton = document.querySelectorAll('.nav__item');
-const btnStart = document.getElementById('button-start')
+
 const gameField = document.createElement('div');
-const btnSimple = document.getElementById('simple');
-const btnMedium = document.getElementById('medium');
-const btnDifficult = document.getElementById('difficult');
 
-navButton[0].classList.add('checked');
-
-let navButtonOnClick = (item) => {
-  item.addEventListener('click', function() {
-    navButton.forEach((el) => {
-      el.classList.remove('checked');
-      this.classList.add('checked');
-    });
-  });
+const stopGame = () => {
+  gameContainer.appendChild(startMenu);
+  gameField.remove();
+  gameField.innerHTML = '';
 }
 
-navButton.forEach(navButtonOnClick);
-
-let btnStartOnClick = () => {
-
-  startMenu.remove();
-  gameContainer.appendChild(gameField);
-  let numberOfCard;
-
-  function getGameField() {
-    let getCards = () => {
-      const card = document.createElement('div');
-      const cardBack = document.createElement('div');
-      const cardFront = document.createElement('div');
-    
-      card.classList.add('flip-card');
-      cardBack.classList.add('flip-card-back');
-      cardFront.classList.add('flip-card-front');
-    
-      gameField.appendChild(card); 
-      card.appendChild(cardFront);
-      card.appendChild(cardBack);
-    }
-
-    if (btnSimple.classList.contains('checked')) {
-      gameField.className = 'three-card';
-      for (i = 0; i < 3; i++) {
-        getCards();
-        numberOfCard = 3;
-      }
-    }
-
-    else if (btnMedium.classList.contains('checked')) {
-      gameField.className = 'six-card';
-      for (i = 0; i < 6; i++) {
-        getCards();
-        numberOfCard = 6;
-      }
-    }
-
-    else {
-      gameField.className = 'ten-card';
-      for (i = 0; i < 10; i++) {
-        getCards();
-        numberOfCard = 10;
-      }
-    }
+const chooseLevel = () => {
+  navItems.forEach(btn => {
+    btn.addEventListener('click', () => {
+      if(!btn.classList.contains('checked')) {
+        deactive();
+        btn.classList.add('checked');
+      } 
+    })
+  })
+  const deactive = () => {
+    navItems.forEach(btn => btn.classList.remove('checked'))
   }
+};
 
-  getGameField() 
+const getCards = () => {
+  const card = document.createElement('div');
+  const cardBack = document.createElement('div');
+  const cardFront = document.createElement('div');
+
+  card.classList.add('flip-card');
+  cardBack.classList.add('flip-card-back');
+  cardFront.classList.add('flip-card-front');
+
+  gameField.appendChild(card); 
+  card.appendChild(cardFront);
+  card.appendChild(cardBack);
+}
+
+const getGameField = (obj) => {
+  gameContainer.appendChild(gameField)
+  obj.numberOfCard && gameField.classList.add(obj.nameOfClass)
+
+  for (let i = 0; i < obj.numberOfCard; i++) {
+    getCards()
+  }
 
   const flipCard = document.querySelectorAll('.flip-card');
-  let randomCard = Math.floor(Math.random() * numberOfCard);
-
-  for (i = 0; i < numberOfCard; i++) {
-    if (i === randomCard) {
-      flipCard[i].firstElementChild.classList.add('winner');
-    } 
-  }
-
-  let getStart = () => { 
-    gameContainer.appendChild(startMenu);
-    gameField.remove();
-    gameField.innerHTML = '';
-  }
-
-  let flipCardOnClick = (elem) => {
-    elem.addEventListener('click', function() {
-      flipCard.forEach(() => {
-        this.classList.add('on-click');
-        flipCard.forEach((item) => item.addEventListener('click', getStart))
-      });
-    });
-  }
-  flipCard.forEach(flipCardOnClick); 
+  const randomCard = Math.floor(Math.random() * obj.numberOfCard);
+  
+  flipCard.forEach((card,i) => {  
+    card.addEventListener('click', () => {
+      card.classList.add('on-click');
+      if(randomCard === i) {
+        card.firstElementChild.classList.add('winner');
+        flipCard.forEach((item) => {
+          item.addEventListener('click', stopGame)
+        })
+      } else {
+        flipCard.forEach((item) => {
+          item.addEventListener('click', stopGame)
+        })
+      }
+    })
+  })
 }
- 
 
-btnStart.addEventListener('click', btnStartOnClick);
+const btnStartOnClick = () => {
+  startMenu.remove();
+  const levels = {};
+
+  navItems.forEach(btn => {
+    if(btn.classList.contains('simple') && btn.classList.contains('checked')) {
+      levels.nameOfClass = 'three-card';
+      levels.numberOfCard = 3;
+    }
+    if(btn.classList.contains('medium') && btn.classList.contains('checked')) {
+      levels.nameOfClass = 'six-card';
+      levels.numberOfCard = 6;
+    }
+    if(btn.classList.contains('difficult') && btn.classList.contains('checked')) {
+      levels.nameOfClass = 'ten-card';
+      levels.numberOfCard = 10; 
+    }
+  })
+  getGameField(levels)
+}
+
+btnStart.addEventListener('click', btnStartOnClick)
+
+chooseLevel()
+
+
+
+
+
+
+
